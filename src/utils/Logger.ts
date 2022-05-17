cccd68e1aa14cfd46dad119fb810ad8d8a9b1fb0
fadiinho <ffadiinho@gmail.com>
2022-05-17 01:21:07 -0300

    Fix logger

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -5,8 +5,8 @@ import pino from 'pino';
 
 const logger = pino({
   transport: {
-    target: './pino-pretty-transport.ts'
+    target: process.env.NODE_ENV === 'production' ? './pino-pretty-transport.js' : './pino-pretty-transport.ts'
   }
-}).child({ level: process.env.ENVIROMENT === 'production' ? 'info' : 'debug' });
+}).child({ level: process.env.NODE_ENV === 'production' ? 'info' : 'debug' });
 
 export default logger;
