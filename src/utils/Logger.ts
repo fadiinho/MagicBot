f789186bff1b2ad9685f48912db6ea2fda2082a1
fadiinho <ffadiinho@gmail.com>
2022-06-17 23:41:11 +0000

    fix: pino logger error

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -3,11 +3,22 @@ dotenv.config();
 
 import pino from 'pino';
 
-const logger = pino({
-  transport: {
-    target: process.env.NODE_ENV === 'production' ? './pino-pretty-transport.js' : './pino-pretty-transport.ts'
-  },
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug'
-});
+import pinoPretty from 'pino-pretty';
 
-export default logger;
+const pinoStream = pinoPretty({
+  messageFormat: '{msg}',
+  customPrettifiers: {
+    time: (timestamp: any) => {
+      const d = new Date(timestamp);
+      const hours = d.getHours();
+      const min = d.getMinutes();
+      const sec = d.getSeconds();
+      return `${hours}:${min}:${sec}`;
+    }
+  }
+})
+
+export const logger = pino({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug'
+}, pinoStream);
+
