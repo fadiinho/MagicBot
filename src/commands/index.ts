379edab804fe82a046479cfb65ac84d8dc235fe7
fadiinho <ffadiinho@gmail.com>
2022-07-27 03:32:19 -0300

    chore: remove useless comments

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,3 @@
-// import GetViewOnce from './GetViewOnce';
-// import Ping from './Ping';
-// import ReactToMsg from './ReactToMsg';
-// import Reply from './Reply';
-// import TagAll from './TagAll';
-// import Commands from './Commands';
-// import AnimeSearch from './AnimeSearch';
-// import TiktokDownload from './TiktokDownload';
-
 export * as GetViewOnce from './GetViewOnce';
 export * as Ping from './Ping';
 export * as ReactToMsg from './ReactToMsg';
